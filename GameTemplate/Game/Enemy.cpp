cfbbbb602410caf27b32a6936904e76834eb3b95
itoYusei <CA01244004@st.kawahara.ac.jp>
2025-09-05 11:28:57 +0900

    Enemyクラスの変更

---
+++ GameTemplate/Game/Enemy.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "Enemy.h"
+
+Enemy::Enemy()
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::Update()
+{
+	
+}
+
+void Enemy::Move()
+{
+	
+}
+
+void Enemy::Rotation()
+{
+	
+}
+
+void Enemy::Render(RenderContext&rc)
+{
+	modelRender.Draw(rc);
+}
